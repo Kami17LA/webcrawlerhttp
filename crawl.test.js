920ea790372b57fe89305289bf4973319e845ab5
Katherine Milagros Quispe Pantoja <katherinebemol17@gmail.com>
2023-02-26 14:12:45 -0500

    NormalizeURL and test

--- crawl.test.js
+++ crawl.test.js
@@ -1,9 +1,34 @@
 const { normalizeURL } = require("./crawl.js");
 const { test, expect } = require("@jest/globals");
 
-test("normalizeURL", () => {
-  const input = "";
+// Test 1
+test("normalizeURL strip protocol", () => {
+  const input = "https://blog.boot.dev/path";
   const actual = normalizeURL(input);
-  const expected = "";
+  const expected = "blog.boot.dev/path";
   expect(actual).toEqual(expected);
+});
+
+// Test 2
+test("normalizeURL strip trailing slash", () => {
+	const input = "https://blog.boot.dev/path/";
+	const actual = normalizeURL(input);
+	const expected = "blog.boot.dev/path";
+	expect(actual).toEqual(expected);
+});
+
+// Test 3
+test("normalizeURL capitals", () => {
+	const input = "https://BLOG.boot.dev/path/";
+	const actual = normalizeURL(input);
+	const expected = "blog.boot.dev/path";
+	expect(actual).toEqual(expected);
+});
+
+// Test 4
+test("normalizeURL strip http", () => {
+	const input = "https://blogs.boot.dev/path/";
+	const actual = normalizeURL(input);
+	const expected = "blog.boot.dev/path";
+	expect(actual).toEqual(expected);
 });
