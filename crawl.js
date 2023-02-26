15bc0c0868806d3f8f36380267d6f9dac451a0d4
Katherine Milagros Quispe Pantoja <katherinebemol17@gmail.com>
2023-02-26 16:28:10 -0500

    getURLsFromHTML test

--- crawl.js
+++ crawl.js
@@ -1,3 +1,23 @@
+const {JSDOM} = require('jsdom')
+
+function getURLsFromHTML(htmlBody, baseURL){
+  const urls = []
+  const dom = new JSDOM(htmlBody)
+  const linkElements = dom.window.document.querySelectorAll('a')
+
+  for(const linkElement of linkElements){
+    if(linkElement.href.slice(0, 1) === '/'){
+      // relative url
+      urls.push(`${baseURL}${linkElement.href}`)
+    } else {
+      // absolute url
+      urls.push(linkElement.href)
+    }
+  }
+  
+  return urls
+}
+
 function normalizeURL(urlString){
   const urlObj = new URL(urlString)
   const hostPath = `${urlObj.hostname}${urlObj.pathname}`
@@ -9,6 +29,7 @@ function normalizeURL(urlString){
 }
 
 module.exports.normalizeURL = normalizeURL;
+module.exports.getURLsFromHTML = getURLsFromHTML;
 
 /* module.exports = (
     normalizeURL
