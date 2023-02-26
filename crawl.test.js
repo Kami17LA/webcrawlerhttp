db46e649286df2b63b0fb7f1e346acc45f75eae6
Katherine Milagros Quispe Pantoja <katherinebemol17@gmail.com>
2023-02-26 13:13:40 -0500

    added jest dep, created main.js

---
+++ crawl.test.js
@@ -0,0 +1,8 @@
+const { normalizeURL } = require('./crawl.js')
+const { test, expect } = require('@jest/globals')
+
+test('normalizeURL', () => {
+	const input = ''
+	const actual = normalizeURL(input)
+	const
+})
